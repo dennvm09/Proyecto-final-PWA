9006c25a9aded544b6dba1ad7cb9ba311861cec1
dennvm09 <valenmosquera1999@gmail.com>
2021-06-09 02:22:47 -0500

    main navbar is almost ready

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,28 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+//  Screens
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+
+//Components
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      {/* Navbar */}
+    <Router>
+      <Navbar />
       {/* SideDrawer */}
       {/* Backdrop */}
-      {/* HomeScreen */}
-      {/* ProductScreen */}
-      {/* CartScreen */}
-      Hello to my shopping cart
-    </div>
+      <main>
+        <Switch>
+          <Route exact path="/" component={HomeScreen} />
+          <Route exact path="/product/:id" component={ProductScreen} />
+          <Route exact path="/cart" component={CartScreen} />
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
