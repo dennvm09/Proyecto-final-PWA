d3932f8d0e09e551650c1e8b2324462f1fa50fab
dennvm09 <valenmosquera1999@gmail.com>
2021-06-08 23:16:53 -0500

    adding some products to db and data import script

---
+++ backend/data/products.js
@@ -0,0 +1,18 @@
+const products = [
+    {
+        name: "Crop top mango",
+        imageUrl: "https://unsplash.com/photos/M7z7ip__K68",
+        description: "Hermoso crop top de algodón. Color rosa curuba. Talla única.",
+        price: 15000,
+        countInStock: 20,
+    },
+    {
+        name: "Jean salvaje",
+        imageUrl: "https://unsplash.com/photos/BciCcl8tjVU",
+        description: "Jean tiro alto. Estilo desgastado. Talla 12.",
+        price: 57000,
+        countInStock: 15,
+    },
+];
+
+module.exports = products;
