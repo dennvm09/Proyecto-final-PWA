9fd06eea429762a532244d37d886e044e19f5498
dennvm09 <valenmosquera1999@gmail.com>
2021-06-09 00:14:33 -0500

    conf App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      {/* Navbar */}
+      {/* SideDrawer */}
+      {/* Backdrop */}
+      {/* HomeScreen */}
+      {/* ProductScreen */}
+      {/* CartScreen */}
       Hello to my shopping cart
     </div>
   );
