55c7af6e176e35cae75c74ebe1f37a945cc688be
dennvm09 <valenmosquera1999@gmail.com>
2021-06-10 01:58:43 -0500

    Everything related to the cart is ready.

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,9 +1,28 @@
 import "./CartScreen.css";
 
+//Components
+import CartItem from "../components/CartItem"
 const CartScreen = () => {
     return (
         <div className="cartscreen">
-            Cart Screen
+            <div className="cartscreen_left">
+                <h2>Shopping Cart</h2>
+
+                <CartItem />
+                <CartItem />
+                <CartItem />
+                <CartItem />
+                <CartItem />
+            </div>
+            <div className="cartscreen_right">
+                <div className="cartscreen_info">
+                    <p>Subtotal (0) items</p>
+                    <p>$15.000</p>
+                </div>
+                <div>
+                    <button>Proceed To Checkout</button>
+                </div>
+            </div>
         </div>
     )
 }
